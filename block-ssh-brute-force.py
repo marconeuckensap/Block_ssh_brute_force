fe05f30ab2ad0ab3ba87b1d505d93d99ed1f64e0
marconeuckensap <marco.neuckens@student.ap.be>
2021-04-14 22:49:41 +0200

    filter ip adresses

--- block-ssh-brute-force.py
+++ block-ssh-brute-force.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3
 import os
+import re
+import ipaddress
+
 sshlog = open("/home/marco/os_scripting/Block_ssh_brute_force/sshdlog", "r")
-print(sshlog.readlines())
+#print(sshlog.readlines())
+
+bad_ip = set() # creating a set for unique ip-adresses
+
+for line in sshlog.readlines():
+    #print("test" + line)
+    if "Invalid user" in line: 
+       #print(line)  # will show login attempts from invalid users
+       ip = str(re.findall("[0-9]*[0-9][.][0-9]*[0-9][.][0-9]*[0-9][.][0-9]*[0-9]", line))
+       #remove []
+       ip = ip.strip("['']")
+       print(ip)
+    
+
+
+
+
+
+
+
+
 sshlog.close()
+
